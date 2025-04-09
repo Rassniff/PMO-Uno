36d0fa31623ead5546739b251c9039d94766201e
DiegoChiodi74 <d.chiodi3@campus.uniurb.it>
2025-04-09 11:41:02 +0200

    prova
    prova

--- uno/src/main/java/com/uno/model/Main.java
+++ uno/src/main/java/com/uno/model/Main.java
@@ -1,7 +1,7 @@
 package com.uno.model;
 
 import java.util.List;
-
+//creazione dei player
 public class Main {
     public static void main(String[] args) {
         Player p1 = new HumanPlayer("Tu");
