b71bf087f37fd9638d08d43827ead02f6e23a9e0
Ursu Andrii <66079622+Rassniff@users.noreply.github.com>
2025-04-09 11:43:33 +0200

    creazione game commento

--- uno/src/main/java/com/uno/model/Main.java
+++ uno/src/main/java/com/uno/model/Main.java
@@ -9,6 +9,7 @@ public class Main {
         Player p3 = new BotPlayer("Bot2");
         Player p4 = new BotPlayer("Bot3");
 
+        //creazione game
         Game game = new Game(List.of(p1, p2, p3, p4));
         game.playGame();
     }
